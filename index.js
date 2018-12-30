6fd48d5bfc717c8e7a49039e596e9d5f06f7e0bd
rudy-stephane <tekamfossi@gmail.com>
2018-12-30 19:24:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const express = require('express')
+var express = require('express')
+var app = express()
+
+const PORT = process.env.PORT || 5000
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+/*const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,4 +19,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
