09ed18a96bb73f91aea486b88f86b1c95507509b
rudy-stephane <tekamfossi@gmail.com>
2018-12-30 23:04:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var app = express()
 
 const PORT = process.env.PORT || 5000
  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 /*app.get('/', function (req, res) {
   res.send(req.body)
  
