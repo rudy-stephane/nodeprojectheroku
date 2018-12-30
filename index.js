f58fc3b2890057d1bdec5438b532e766537acebf
rudy-stephane <tekamfossi@gmail.com>
2018-12-30 23:01:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
  
 app.post('/', function (req, res) {
   //res.send(req.body)
- console.log(JSON.parse(req.body))
+ console.log(typeof(req.body))
  res.end("yes")
 })
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
