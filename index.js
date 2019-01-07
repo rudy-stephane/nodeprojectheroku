f1371962c179755256f9e1e68fca646ee12ddb19
rudy-stephane <tekamfossi@gmail.com>
2019-01-08 00:38:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ app.post('/webhook', function(req,res){
 
 })
 app.post('/', function (req, res) {
-  if(req.body.rep == 'sendfile'){
+  //if(req.body.rep == 'sendfile'){
   	console.log(req.body.rep+'  valeur cherchée')
-  }
+  //}
 	//console.log(process.cwd())
     //res.setHeader('Content-Type', 'application/json');
  //console.log(fs.exists(path.join(process.cwd(),'rudystephane.pdf')))
