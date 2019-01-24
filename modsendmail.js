a28d27bb019e9bd7ff53babbb0d5ca7a62fac1dd
rudy-stephane <tekamfossi@gmail.com>
2019-01-24 12:54:42 +0100

    Update modsendmail.js

--- modsendmail.js
+++ modsendmail.js
@@ -8,7 +8,7 @@ exports.fcsendmail = function(from, to, subject ,message, attachfile){
 			secure:false,
 			  auth: {
 				user: 'venceslas.tchekou@netinafrica.com',
-				pass: '#' 
+				pass: 'Cameroun237#' 
 			  }
 			});
 		var mailOptions = {
